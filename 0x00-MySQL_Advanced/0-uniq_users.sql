1ad042844c8d5e55cd62973515b9ba18abfe9e12
Sgechuki <sgechuki@outlook.com>
2024-05-09 19:32:48 +0000

    Add NOT NULL

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,7 @@
 -- 0 We are all unique
 -- creates a table users
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) UNIQUE,
-	name VARCHAR(255),
-	PRIMARY KEY (id)
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 	);
-
