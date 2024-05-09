93194a352a713a9ed53d28f1c4fcbb1789e654d1
Sgechuki <sgechuki@outlook.com>
2024-05-09 20:29:25 +0000

    COALESCE

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, COALESCE(split, 2022) - formed lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY 2 DESC;
