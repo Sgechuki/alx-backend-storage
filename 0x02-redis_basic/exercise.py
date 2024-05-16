815883f608cf0ca9c2e85b4ef986bd6bcd2a956b
Sgechuki <sgechuki@outlook.com>
2024-05-16 01:50:19 +0000

    Redis in python

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module holds the clas Cache
+"""
+import redis
+from typing import Union, Callable, Optional, Any
+import uuid
+
+
+class Cache():
+    """
+    store an instance of the Redis client
+    as a private variable named _redis
+    flush the instance using
+    """
+
+    def __init__(self):
+        """Initialize"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """takes a data argument and
+        returns a string"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key, fn: Optional[Callable[[bytes], Any]] = None) -> Any:
+        """convert the data back to the desired format"""
+        i = self._redis.get(key)
+        if i is None:
+            return i
+        if fn is not None:
+            return fn(i)
+        return i
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Convert byte to string"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Convert byte to int"""
+        return self.get(key, lambda x: int(x))
