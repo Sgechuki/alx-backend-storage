d26c01f1b8e1b5e2b92986b32aadccd0d7e5fc51
Sgechuki <sgechuki@outlook.com>
2024-05-16 02:32:07 +0000

    Call counts decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,9 +2,19 @@
 """
 Module holds the clas Cache
 """
+from functools import wraps
 import redis
 from typing import Union, Callable, Optional, Any
 import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache():
@@ -19,6 +29,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data argument and
         returns a string"""
